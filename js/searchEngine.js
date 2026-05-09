5b45e1c200995a53ba7395b54ebde13ebf3c84d4
Pablo Bella <pablo.s.bella@gmail.com>
2026-05-09 16:26:42 -0300

    Refactor index construction to use batching

--- js/searchEngine.js
+++ js/searchEngine.js
@@ -1,48 +1,46 @@
-// Índice de búsqueda
-
-import { normalizarTexto } from './utils.js';
-
-let indiceBusqueda = {};
-let medicamentosGlobales = [];
-
 export function construirIndice(medicamentos) {
+    // Usar requestIdleCallback para no bloquear
+    if ('requestIdleCallback' in window) {
+        requestIdleCallback(() => construirIndiceAsync(medicamentos));
+    } else {
+        setTimeout(() => construirIndiceAsync(medicamentos), 100);
+    }
+}
+
+function construirIndiceAsync(medicamentos) {
     medicamentosGlobales = medicamentos;
     indiceBusqueda = {};
     
-    medicamentos.forEach((med, idx) => {
-        const textoCompleto = med.DROGA_buscar + ' ' + med.MARCA_buscar + ' ' + med.LABORATORIO_buscar;
-        const palabras = textoCompleto.split(/\s+/);
-        
-        palabras.forEach(palabra => {
-            if (palabra.length < 2) return;
+    // Procesar en lotes de 500 para no bloquear
+    const batchSize = 500;
+    let index = 0;
+    
+    function processBatch() {
+        const end = Math.min(index + batchSize, medicamentos.length);
+        for (let i = index; i < end; i++) {
+            const med = medicamentos[i];
+            const textoCompleto = med.DROGA_buscar + ' ' + med.MARCA_buscar + ' ' + med.LABORATORIO_buscar;
+            const palabras = textoCompleto.split(/\s+/);
             
-            if (!indiceBusqueda[palabra]) {
-                indiceBusqueda[palabra] = new Set();
-            }
-            indiceBusqueda[palabra].add(idx);
-            
-            for (let i = 2; i <= palabra.length; i++) {
-                const fragmento = palabra.substring(0, i);
-                if (!indiceBusqueda[fragmento]) {
-                    indiceBusqueda[fragmento] = new Set();
+            palabras.forEach(palabra => {
+                if (palabra.length < 2) return;
+                if (!indiceBusqueda[palabra]) indiceBusqueda[palabra] = new Set();
+                indiceBusqueda[palabra].add(i);
+                
+                for (let j = 2; j <= palabra.length; j++) {
+                    const fragmento = palabra.substring(0, j);
+                    if (!indiceBusqueda[fragmento]) indiceBusqueda[fragmento] = new Set();
+                    indiceBusqueda[fragmento].add(i);
                 }
-                indiceBusqueda[fragmento].add(idx);
-            }
-        });
-    });
+            });
+        }
+        index = end;
+        if (index < medicamentos.length) {
+            setTimeout(processBatch, 50);
+        } else {
+            console.log('Índice construido con', medicamentos.length, 'medicamentos');
+        }
+    }
     
-    console.log('Índice construido con', medicamentos.length, 'medicamentos');
-}
-
-export function buscarMedicamentos(texto) {
-    if (!texto || texto.length < 2) return [...medicamentosGlobales];
-    
-    const textoNormalizado = normalizarTexto(texto);
-    const resultadosSet = indiceBusqueda[textoNormalizado] || new Set();
-    
-    return [...resultadosSet].map(idx => medicamentosGlobales[idx]);
-}
-
-export function getMedicamentosGlobales() {
-    return medicamentosGlobales;
+    processBatch();
 }
